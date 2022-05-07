32f0ab2f8d2e6cf1c5f41739a6ea233412ced9bc
mtfbs <mateusfbsoares@gmail.com>
2022-05-07 10:54:09 -0300

    latest udp e rdt

--- utility/RDT3.py
+++ utility/RDT3.py
@@ -1,21 +1,58 @@
-import socket
+from torch import reciprocal
+from UDP import udp_connetion
 
 
-def make_pkt():
-    pass
+class RDT:
+    def __init__(self, type, port):
+        '''
+            type: "client" ou "server"
+        '''
+        self.udp_connection = udp_connetion(type=type, port=port)
+        self.sequence_number = 0
+        self.data_buffer = None
 
+    def make_pkt(self, data, checksum, sequence_number):
+        return {"data": data,  "checksum": checksum,  "sequence_number": sequence_number}
 
-def make_ack():
-    pass
+    def make_ack(self, ack, checksum):
+        return {"ack": ack, "checksum": checksum}
 
+    def is_corrupt(self, ):
+        pass
 
-def isCorrupt():
-    pass
+    def recieve(self, ):
+        pass
 
+    def send(self, msg_string):
+        # creates the pkt
+        pkt_to_send = self.make_pkt(self.seq_num, msg_string)
 
-def rdt3_send():
-    pass
+        # increments the sequence number
+        self.seq_num += 1
 
+        while True:
+            ### sends the packet ###
+            self.udp_connection.send(pkt_to_send.encode())
 
-def rdt3_recieve():
-    pass
+            # clears the data buffer
+            self.data_buffer = None
+
+            # variable for listening for incoming data
+            recieved_pkt = None
+
+            ### loops until recieves data ###
+            while recieved_pkt == None:
+                recieved_pkt = self.udp_connection.recieve().decode()
+
+            # sets byte_buffer to received data
+            self.data_buffer = recieved_pkt
+
+            ### checks for corruption ###
+            # if data is not corrupted, check sequence numbers
+            if(not self.is_corrupt(recieved_pkt)):
+
+                ### check sequence numbers ###
+                # check to make sure the response sequence number isn't behind
+                if recieved_pkt["sequence_number"] < self.sequence_number:
+                    # creates ack pkt
+                    ack = self.make_ack(1,)
--- utility/UDP.py
+++ utility/UDP.py
@@ -1,9 +1,41 @@
+from logging import exception
 import socket
 
 
-def udp_send():
-    pass
+class udp_connetion:
+    def __init__(self, type, port):
+        '''
+            type: "client" ou "server"
+        '''
 
+        if(type == "server"):
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.socket.bind(("localhost", port))
+            print(f"UDP Server is listening on port {port}")
 
-def udp_recieve():
-    pass
+        elif(type == "client"):
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        else:
+            raise Exception(
+                f"{type} is not a valid type. Must be either 'client' or 'server'.")
+
+    def recieve(self):
+        data, adress = self.socket.recvfrom(2048)
+        return (data, adress)
+
+    def send(self, data, reciever_ip):
+        '''
+            data must be in bytes (encoded)
+        '''
+        self.socket.sendto(data, ("localhost", reciever_ip))
+        if(type == "client"):
+            print(
+                f"Client: {data.decode()} ({data})")
+        elif(type == "server"):
+            print(
+                f"Server: {data.decode()} ({data})")
+
+    def close(self):
+        self.socket.close()
+        print("Client closed")
